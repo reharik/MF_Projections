81b068b5d6b13a20d5b52bba81197635061064e3
Raif Harik <reharik@gmail.com>
2015-10-01 15:27:51 -0500

    fucking more fuck

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ var extend = require('extend');
 var config = require('config');
 
 module.exports = function(_options) {
-    var options = {};
+    var options = {
+        dagon:{
+            application:'projections'
+        }
+    };
     extend(options, config.get('configs') || {}, _options || {});
     var container = require('./registry')(options);
 
