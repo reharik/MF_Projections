75188ac1415a55a69c3ee60a69830223be84eb6d
Raif Harik <reharik@gmail.com>
2017-01-21 19:23:16 -0600

    adding updateAppointment stuff

--- app/src/EventHandlers/AppointmentEventHandler.js
+++ app/src/EventHandlers/AppointmentEventHandler.js
@@ -3,8 +3,54 @@ module.exports = function(rsRepository, logger) {
         logger.info('AppointmentEventHandler started up');
 
         async function appointmentScheduled(event) {
-                        
+
             var sql = `INSERT INTO "appointment" (
+            "id", 
+            "date",
+            "trainer",
+            "document"
+            ) VALUES (
+            '${ event.id }',
+            '${ event.localDate }',
+            '${ event.trainer }',
+            '${JSON.stringify(event)}')`;
+            return await rsRepository.saveQuery(sql);
+        }
+
+        async function appointmentCanceled(event) {
+
+            var sql = `DELETE FROM "appointment" (
+            "id", 
+            "date",
+            "trainer",
+            "document"
+            ) VALUES (
+            '${ event.id }',
+            '${ event.localDate }',
+            '${ event.trainer }',
+            '${JSON.stringify(event)}')`;
+            return await rsRepository.saveQuery(sql);
+        }
+
+        async function appointmentMovedFromDifferentDay(event) {
+            return appointmentUpdated(event);
+        }
+
+        async function appointmentTypeChanged(event) {
+            return appointmentUpdated(event);
+        }
+
+        async function clientsChangedForAppointment(event) {
+            return appointmentUpdated(event);
+        }
+
+        async function timeChangedForAppointment(event) {
+            return appointmentUpdated(event);
+        }
+
+        async function appointmentUpdated(event) {
+
+            var sql = `update "appointment" (
             "id", 
             "date",
             "trainer",
