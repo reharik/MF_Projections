c6df3b6ec33e6af75abf9dc8e9f6e703ea665eee
Raif Harik <reharik@gmail.com>
2017-03-29 11:47:06 -0500

    debugging

--- app/src/EventHandlers/TrainerEventHandler.js
+++ app/src/EventHandlers/TrainerEventHandler.js
@@ -15,7 +15,9 @@ module.exports = function(rsRepository, moment, logger) {
                 color        : event.color,
                 clients     : event.clients
             };
-
+console.log(`==========trainer=========`);
+console.log(trainer);
+console.log(`==========END trainer=========`);
             return await rsRepository.save('trainer', trainer);
         }
 
